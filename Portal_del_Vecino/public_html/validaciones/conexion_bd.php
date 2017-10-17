a6fc7ef753da4fc8fbe642a3f9d219aaa93582e2
Matias Mellado <Matias Mellado@DESKTOP-DH23MG0>
2017-10-17 10:36:00 -0300

    Correccion de utf-8

--- Portal_del_Vecino/public_html/validaciones/conexion_bd.php
+++ Portal_del_Vecino/public_html/validaciones/conexion_bd.php
@@ -6,7 +6,7 @@
     $password   = "";
 
     try{
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e){
         echo "Conexion fallida: ". $e->getMessage();

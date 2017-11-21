02bfc73c34ce819c1588757aa1042717fbd51abf
Matias Mellado <Matias Mellado@DESKTOP-DH23MG0>
2017-11-21 10:52:40 -0300

    Correcion en Tesoreria/recursos

--- Portal_del_Vecino/public_html/modulos/tesoreria/tesoreria_recursos.php
+++ Portal_del_Vecino/public_html/modulos/tesoreria/tesoreria_recursos.php
@@ -72,14 +72,14 @@
                             $usuarios = array();
                             $idusuerios = array();
                             $solicitudes = "";
-                            $stmt = $querys->prestamos($result['ID_RECURSO']);
-                            while ($data = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                            $stmt2 = $querys->prestamos($result['ID_RECURSO']);
+                            while ($data = $stmt2->fetch(PDO::FETCH_ASSOC)) {
                                 array_push($idusuerios, ($data['ID_USUARIO']));
                                 array_push($usuarios, (" Desde ".$data['FECHA_INICIO']." Hasta ".$data['FECHA_TERMINO']));
                             }
                             foreach ($idusuerios as &$user){
-                                $stmt = $querys->nombre_usr($user);
-                                $res = $stmt->fetch(PDO::FETCH_ASSOC);
+                                $stmt3 = $querys->nombre_usr($user);
+                                $res = $stmt3->fetch(PDO::FETCH_ASSOC);
                                 $solicitudes = $solicitudes."<li>".$res['NOMBRE']." ".$res['APELLIDO'].$usuarios[$user]."</li>";
                             }
                             if($solicitudes == ""){$solicitudes = "No hay usuarios que hayan solicitado este recurso aún";}
